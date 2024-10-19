b245bf4d149eb665df58a407df98324b26f02cf4
zachtschetter <zachary.tschetter@usd.edu>
2024-10-19 14:11:36 -0500

    go

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,14 +1,12 @@
+"use strict";
 const bulb = `💡`;
-const bulb_div = document.getElementById('lightbulb')
+const bulb_div = document.getElementById('lightbulb');
 let pid = null;
-let vals : number[] = [.2,.3,.7,.8]
-
-
-
-function flip() : string{
-    if(bulb_div!.innerHTML == bulb) return bulb_div!.innerHTML='';
-    return bulb_div!.innerHTML=bulb;
-
+let vals = [.2, .3, .7, .8];
+function flip() {
+    if (bulb_div.innerHTML == bulb)
+        return bulb_div.innerHTML = '';
+    return bulb_div.innerHTML = bulb;
 }
 // <div class="bar red">.60</div>
 //     <div class="bar yellow">.50</div>
@@ -16,36 +14,30 @@ function flip() : string{
 //     <div class="bar green">.30</div>
 //     <div class="bar yellow">.20</div>
 //     <div class="bar red">.10</div>
-
 // let points = document.getElementById("points-div")!
-
-const pointsDiv =document.getElementById("points-div")!
-pointsDiv.innerHTML='';
-function addPoint(){
+const pointsDiv = document.getElementById("points-div");
+pointsDiv.innerHTML = '';
+function addPoint() {
     // vals.shift(); vals.push(Number(document.getElementById('code')!?.innerHTML))
-    const ans = vals.map((v,i)=>`<div class='point' style='margin-left:${(i+1)*125}px; top:${mapValueToPixel(v)}px'></div>`)
-    console.log(ans,vals)
-    pointsDiv.innerHTML=ans.join('')
+    const ans = vals.map((v, i) => `<div class='point' style='margin-left:${(i + 1) * 125}px; top:${mapValueToPixel(v)}px'></div>`);
+    console.log(ans, vals);
+    pointsDiv.innerHTML = ans.join('');
     // const ar  = vals.map((v,i)=>`<div class='point' style='left:${i*50}px></div>"`)
     // console.log(ar)
     // document.getElementById("points-div")!.innerHTML=ar.join('');
-
 }
-
-addPoint()
-function mapValueToPixel(value:number):number {
+addPoint();
+function mapValueToPixel(value) {
     const maxValue = 0.6;
     const maxPixel = 290;
-    
     // Ensure the value is within the expected range
-    if (value < 0) value = 0;
-    if (value > maxValue) value = maxValue;
-
+    if (value < 0)
+        value = 0;
+    if (value > maxValue)
+        value = maxValue;
     // Map the value to pixel position
     return maxPixel - (value / maxValue * maxPixel);
 }
-
-
 // function scale(x:number):number{
 //     if(x>.6) return 0;
 //     if(x<0) return 290;
@@ -55,23 +47,13 @@ function mapValueToPixel(value:number):number {
 //     const div = `<div class="point" style="margin-left: 650px; top: 0px;"></div>`;
 //     return 
 // }
-
 // addPoint()
-
-
-function panic(){
-    bulb_div!.style.background='red'
-
-
-    pid = setInterval(()=>{flip()},300)
+function panic() {
+    bulb_div.style.background = 'red';
+    pid = setInterval(() => { flip(); }, 300);
 }
-
-
-function calm(){
-    bulb_div!.style.background='transparent'
+function calm() {
+    bulb_div.style.background = 'transparent';
 }
-
-
 // panic()
 // calm()
-
